8b0ed5f7d6677e9e8a3f6cf9a579a95d1438b595
Andy-85 <abisegna1985gmail.com>
2020-10-14 17:01:32 +0200

    Committed Let's Review Exercise HackerRank

---
+++ HacherRank_Algorithm/src/LetsReview.java
@@ -0,0 +1,35 @@
+/*HackerRank Algorithm - Let's Review 
+
+Given a string,S , of length N that is indexed from 0 to N - 1 , print its even-indexed and odd-indexed 
+characters as 2 space-separated strings on a single line (see the Sample below for more detail).
+*/
+
+
+
+import java.util.Scanner;
+
+public class LetsReview {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String str;
+		System.out.println("Give the string");
+		str = input.nextLine();
+		for(int i = 0; i < str.length(); i++) {
+			if(i % 2 == 0) {
+				System.out.print(str.charAt(i));
+			}
+		}
+		System.out.print(" ");
+		for(int i = 0; i < str.length(); i++) {
+			if(i % 2 != 0) {
+				System.out.print(str.charAt(i));
+			}
+		}
+		
+		System.out.println();
+		input.close();
+
+	}
+
+}
